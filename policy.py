f6db36c862360d7953d9594568fd11fed52bac18
Artjom Valdas <artjom.valdas@ut.ee>
2021-12-02 16:07:42 +0200

    Finding the nearest person by summing taxi location, person.s and person.t

--- policy.py
+++ policy.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from person import *
 from car import *
-import itertools
 
 
 class Policy(ABC):
@@ -78,72 +77,87 @@ class TakeUpTo4Policy(Policy):
         return np.array([closest_distances[closest_person_indexes.index(personIdx)] if personIdx in closest_person_indexes else np.inf for personIdx in range(len(self.people))])
 
 
-class Nearest4Policy(Policy):
-
-    def get_all_permutations(self, ways_len):
-        answer = []
-        for perm in list(itertools.permutations([j for j in range(ways_len)])):
-            correct = set()
-            passed = True
-            for i in perm:
-                if i // (ways_len / 2) == 1 and i % (ways_len / 2) not in correct:
-                    passed = False
-                    break
-                if i // (ways_len / 2) == 0:
-                    correct.add(i % (ways_len / 2))
-            if passed:
-                answer.append(perm)
-        return answer
-
-    def get_order(self, car_id, closest_person_indexes):
-        ways = dict()
-        for index in closest_person_indexes:
-            person = self.people[index]
-            ways[person.__repr__() + "|s"] = person.s
-        for index in closest_person_indexes:
-            person = self.people[index]
-            ways[person.__repr__() + "|t"] = person.t
-        best_order = self.best_way(self.cars[car_id].at, ways)
-        best_order = self.extract_indexes(best_order)
-        return np.array([self.distances[self.cars[car_id].at, self.people[personIdx].where_to()] if personIdx in best_order else np.inf for personIdx in range(len(self.people))])
-
-    def best_way(self, start, ways):
-        keys = list(ways.keys())
-        best_order = []
-        best_distance = np.inf
-
-        for possibility in self.get_all_permutations(len(ways)):
-            distance = 0
-            starting = start
-            for index in possibility:
-                distance += self.distances[starting, ways[keys[index]]]
-                starting = ways[keys[index]]
-            if best_distance > distance:
-                best_distance = distance
-                best_order = possibility
-        return [keys[key] for key in best_order]
-
-    def extract_indexes(self, persons):
-        result = []
-        for person in persons:
-            if int(person.split("|")[0].split(" ")[1]) not in result:
-                result.append(int(person.split("|")[0].split(" ")[1]))
-        return result
+class NearestSTPolicy(Policy):
 
     def _score(self, car_id: int, time: int) -> np.array:
-        if car_id == 0 and self.cars[car_id].passengers.count(None) != 4:
-            closest_person_indexes = [passenger.id for passenger in self.cars[car_id].passengers if passenger is not None]
-            return self.get_order(car_id, closest_person_indexes)
+        score = []
+        car_location = self.cars[car_id].at
+        for person in self.people:
+            if person.where_to() is not None:
+                if person.state == PersonState.waiting or person.state == PersonState.assigned:
+                    score.append(self.distances[(car_location, person.s)] + self.distances[(person.s, person.t)])
+                else:
+                    score.append(self.distances[(car_location, person.where_to())])
+            else:
+                score.append(np.inf)
+        return np.array(score)
 
-        closest_distances = [np.inf] * (4 if len(self.people) > 4 else len(self.people))
-        closest_person_indexes = [-1] * (4 if len(self.people) > 4 else len(self.people))
-
-        for personIdx in range(len(self.people)):
-            if self.people[personIdx].where_to() is None:
-                continue
-            if self.distances[(self.cars[car_id].at, self.people[personIdx].where_to())] < max(closest_distances):
-                index = closest_distances.index(max(closest_distances))
-                closest_distances[index] = self.distances[(self.cars[car_id].at, self.people[personIdx].where_to())]
-                closest_person_indexes[index] = personIdx
-
-        return self.get_order(car_id, closest_person_indexes)
+# class Nearest4Policy(Policy):
+#
+#     def get_all_permutations(self, ways_len):
+#         answer = []
+#         for perm in list(itertools.permutations([j for j in range(ways_len)])):
+#             correct = set()
+#             passed = True
+#             for i in perm:
+#                 if i // (ways_len / 2) == 1 and i % (ways_len / 2) not in correct:
+#                     passed = False
+#                     break
+#                 if i // (ways_len / 2) == 0:
+#                     correct.add(i % (ways_len / 2))
+#             if passed:
+#                 answer.append(perm)
+#         return answer
+#
+#     def get_order(self, car_id, closest_person_indexes):
+#         ways = dict()
+#         for index in closest_person_indexes:
+#             person = self.people[index]
+#             ways[person.__repr__() + "|s"] = person.s
+#         for index in closest_person_indexes:
+#             person = self.people[index]
+#             ways[person.__repr__() + "|t"] = person.t
+#         best_order = self.best_way(self.cars[car_id].at, ways)
+#         best_order = self.extract_indexes(best_order)
+#         return np.array([self.distances[self.cars[car_id].at, self.people[personIdx].where_to()] if personIdx in best_order else np.inf for personIdx in range(len(self.people))])
+#
+#     def best_way(self, start, ways):
+#         keys = list(ways.keys())
+#         best_order = []
+#         best_distance = np.inf
+#
+#         for possibility in self.get_all_permutations(len(ways)):
+#             distance = 0
+#             starting = start
+#             for index in possibility:
+#                 distance += self.distances[starting, ways[keys[index]]]
+#                 starting = ways[keys[index]]
+#             if best_distance > distance:
+#                 best_distance = distance
+#                 best_order = possibility
+#         return [keys[key] for key in best_order]
+#
+#     def extract_indexes(self, persons):
+#         result = []
+#         for person in persons:
+#             if int(person.split("|")[0].split(" ")[1]) not in result:
+#                 result.append(int(person.split("|")[0].split(" ")[1]))
+#         return result
+#
+#     def _score(self, car_id: int, time: int) -> np.array:
+#         if car_id == 0 and self.cars[car_id].passengers.count(None) != 4:
+#             closest_person_indexes = [passenger.id for passenger in self.cars[car_id].passengers if passenger is not None]
+#             return self.get_order(car_id, closest_person_indexes)
+#
+#         closest_distances = [np.inf] * (4 if len(self.people) > 4 else len(self.people))
+#         closest_person_indexes = [-1] * (4 if len(self.people) > 4 else len(self.people))
+#
+#         for personIdx in range(len(self.people)):
+#             if self.people[personIdx].where_to() is None:
+#                 continue
+#             if self.distances[(self.cars[car_id].at, self.people[personIdx].where_to())] < max(closest_distances):
+#                 index = closest_distances.index(max(closest_distances))
+#                 closest_distances[index] = self.distances[(self.cars[car_id].at, self.people[personIdx].where_to())]
+#                 closest_person_indexes[index] = personIdx
+#
+#         return self.get_order(car_id, closest_person_indexes)
