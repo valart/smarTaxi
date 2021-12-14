02a2314560c79d3de42448aa1e905cf9a863dd27
pchizhov <chizhov.pasha@yandex.ru>
2021-12-14 12:15:34 +0200

    Cleaned up final code and notebook

--- policy.py
+++ policy.py
@@ -27,7 +27,7 @@ class DummyPolicy(Policy):
         return np.random.rand(len(self.people))
 
 
-class ClosestPolicy(Policy):
+class NearestPolicy(Policy):
 
     def _score(self, car_id: int, time: int) -> np.array:
         v1 = self.cars[car_id].at
@@ -38,3 +38,56 @@ class QueuePolicy(Policy):
 
     def _score(self, car_id: int, time: int) -> np.array:
         return np.array(range(len(self.people)), dtype=np.float)
+
+
+class NearestWithDistPolicy(Policy):
+
+    def _score(self, car_id: int, time: int) -> np.array:
+        score = []
+        car_location = self.cars[car_id].at
+        for person in self.people:
+            if person.where_to() is not None:
+                if person.state == PersonState.waiting or person.state == PersonState.assigned:
+                    score.append(self.distances[(car_location, person.s)] + self.distances[(person.s, person.t)])
+                else:
+                    score.append(self.distances[(car_location, person.where_to())])
+            else:
+                score.append(np.inf)
+        return np.array(score)
+
+
+class DiscountedNearestPolicy(Policy):
+    
+    def _score(self, car_id: int, time: int) -> np.array:
+        v1 = self.cars[car_id].at
+        return np.array([self.distances[(v1, p.where_to())] / (time - p.time_init + 1) if p.where_to() is not None else np.inf for p in self.people])
+
+    
+class DiscountedNearestWithDistPolicy(Policy):
+
+    def _score(self, car_id: int, time: int) -> np.array:
+        score = []
+        car_location = self.cars[car_id].at
+        for person in self.people:
+            if person.where_to() is not None:
+                wait_time = (time - person.time_init) if (time - person.time_init) > 0 else 1
+                if person.state == PersonState.waiting or person.state == PersonState.assigned:
+                    score.append((self.distances[(car_location, person.s)] + self.distances[(person.s, person.t)]) / (wait_time))
+                else:
+                    score.append((self.distances[(car_location, person.where_to())]) / (wait_time))
+            else:
+                score.append(np.inf)
+        return np.array(score)
+
+
+class WeightedNearestWithDistPolicy(Policy):
+    
+    def _score(self, car_id: int, time: int) -> np.array:
+        score = []
+        car_loc = self.cars[car_id].at
+        for person in self.people:
+            if person.where_to() is not None:
+                score.append((self.distances[(car_loc,person.s)]+self.distances[(person.s,person.t)])*np.exp(-1/self.distances[(person.s,person.t)]))
+            else:
+                score.append(np.inf)
+        return np.array(score)
